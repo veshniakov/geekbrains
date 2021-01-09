fca1d98b5f611fa6572a93ad55e41e1636d92339
veshniakov <veshniakov@ya.ru>
2021-01-09 15:33:01 +0300

    complete first task of hw4

--- HomeWorks/L4_HW4.playground/Contents.swift
+++ HomeWorks/L4_HW4.playground/Contents.swift
@@ -1,3 +1,51 @@
 import UIKit
+enum Vendor {
+    case Volvo, BMW
+}
+enum Engine: String {
+    case start = "заведен"
+    case stop = "выключен"
+}
+enum Windows: String {
+    case open = "закрыты"
+    case close = "открыты"
+}
+enum EngineType {
+    case Diesel, Gasoline
+}
 
-
+class Car{
+    let vendor: Vendor
+    let yearMake: Int
+    let engineType: EngineType
+    let enginePower: Int
+    var egineStatus: Engine
+    var windowsStatus: Windows
+    
+    func changeWindowsStatus(action: Windows){
+        switch action {
+        case Windows.close:
+            self.windowsStatus = .close
+        default:
+            self.windowsStatus = .open
+        }
+    }
+    
+    func changeEngineStatus(action: Engine){
+        switch action {
+        case Engine.stop:
+            self.egineStatus = .stop
+        default:
+            self.egineStatus = .start
+        }
+    }
+    
+    init(vendor: Vendor, yearMake: Int, engineType: EngineType, enginePower: Int, egineStatus: Engine, windowsStatus: Windows) {
+        self.vendor = vendor
+        self.yearMake = yearMake
+        self.engineType = engineType
+        self.enginePower = enginePower
+        self.egineStatus = egineStatus
+        self.windowsStatus = windowsStatus
+    }
+}

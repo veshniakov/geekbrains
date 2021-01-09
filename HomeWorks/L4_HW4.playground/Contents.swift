5a65c902ee9a548ed1cd7229cba8230ccbc9f2aa
veshniakov <veshniakov@ya.ru>
2021-01-09 16:55:27 +0300

    complete HW4

--- HomeWorks/L4_HW4.playground/Contents.swift
+++ HomeWorks/L4_HW4.playground/Contents.swift
@@ -7,18 +7,19 @@ enum Engine: String {
     case stop = "выключен"
 }
 enum Windows: String {
-    case open = "закрыты"
-    case close = "открыты"
+    case open = "открыты"
+    case close = "закрыты"
 }
-enum EngineType {
-    case Diesel, Gasoline
+enum EngineType: String {
+    case Diesel = "дизельный"
+    case Gasoline = "бензиновый"
 }
 enum BackState: String {
-    case up = "кузов опрокинут"
-    case down = "кузов опущен"
+    case up = "опрокинут"
+    case down = "опущен"
 }
 enum SportMode {
-    case standart, sport
+    case Standart, Sport
 }
 
 
@@ -59,12 +60,12 @@ class Car{
 }
 
 class TrunkCar: Car{
-    var backState = BackState
+    var backState: BackState
     init(vendor: Vendor, yearMake: Int, engineType: EngineType, enginePower: Int, egineStatus: Engine, windowsStatus: Windows, backState: BackState){
         self.backState = backState
+        super.init(vendor: vendor, yearMake: yearMake, engineType: engineType, enginePower: enginePower, egineStatus: egineStatus, windowsStatus: windowsStatus)
     }
-    super.init(vendor: Vendor, yearMake: Int, engineType: EngineType, enginePower: Int, egineStatus: Engine, windowsStatus: Windows)
-    
+
     func changeBackState(action: BackState){
         switch action {
         case BackState.up:
@@ -76,18 +77,29 @@ class TrunkCar: Car{
 }
 
 class SportCar: Car {
-    var transsmissionMode = SportMode
+    var transsmissionMode: SportMode
     init(vendor: Vendor, yearMake: Int, engineType: EngineType, enginePower: Int, egineStatus: Engine, windowsStatus: Windows, transsmissionMode: SportMode ) {
         self.transsmissionMode = transsmissionMode
+        super.init(vendor: vendor, yearMake: yearMake, engineType: engineType, enginePower: enginePower, egineStatus: egineStatus, windowsStatus: windowsStatus)
     }
-    super.init(vendor: Vendor, yearMake: Int, engineType: EngineType, enginePower: Int, egineStatus: Engine, windowsStatus: Windows)
     
     func changeTranssmissionMode(mode: SportMode){
         switch mode {
-        case SportMode.standart:
-            self.SportMode = .standart
+        case SportMode.Standart:
+            self.transsmissionMode = .Standart
         default:
-            self.SportMode = .sport
+            self.transsmissionMode = .Sport
         }
     }
 }
+var bmwM6 = SportCar(vendor: .BMW, yearMake: 2006, engineType: .Gasoline, enginePower: 507, egineStatus: .stop, windowsStatus: .close, transsmissionMode: .Standart)
+print("Автомобиль \(bmwM6.egineStatus.rawValue), окна \(bmwM6.windowsStatus.rawValue), коробка передач в режиме \(bmwM6.transsmissionMode). Автомобиль имеет \(bmwM6.engineType.rawValue) двигатель, мощностью \(bmwM6.enginePower) л.с.")
+bmwM6.changeEngineStatus(action: .start)
+bmwM6.changeWindowsStatus(action: .open)
+bmwM6.changeTranssmissionMode(mode: .Sport)
+print("Автомобиль \(bmwM6.egineStatus.rawValue), окна \(bmwM6.windowsStatus.rawValue), коробка передач в режиме \(bmwM6.transsmissionMode). Автомобиль имеет \(bmwM6.engineType.rawValue) двигатель, мощностью \(bmwM6.enginePower) л.с.")
+var volvoFMX = TrunkCar(vendor: .Volvo, yearMake: 2012, engineType: .Diesel, enginePower: 357, egineStatus: .stop, windowsStatus: .close, backState: .down)
+print("Грузовик \(volvoFMX.vendor) готов к работе. Двигатель \(volvoFMX.egineStatus.rawValue), кузов \(volvoFMX.backState.rawValue).")
+volvoFMX.changeBackState(action: .up)
+volvoFMX.changeEngineStatus(action: .start)
+print("Разгрузка окончена. Двигатель \(volvoFMX.egineStatus.rawValue), кузов \(volvoFMX.backState.rawValue).")

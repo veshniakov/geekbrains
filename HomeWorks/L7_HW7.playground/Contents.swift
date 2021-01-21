1ba1136a2caa0c7b8affe026ee4ca6d2a17d8857
veshniakov <veshniakov@ya.ru>
2021-01-21 19:39:00 +0300

    complete HW7

--- HomeWorks/L7_HW7.playground/Contents.swift
+++ HomeWorks/L7_HW7.playground/Contents.swift
@@ -1,3 +1,50 @@
-import UIKit
+enum PossibleErrors: Error {
+  case notInStock
+  case notEnoughMoney
+}
 
-var str = "Hello, playground"
+struct Drink {
+  let price: Int
+  var count: Int
+}
+
+
+class Fridge {
+
+  var deposit = 11
+  var drinksInFridge = ["Pepsi": Drink(price: 10, count: 1), "Lipton": Drink(price: 11, count: 0), "Mirinda": Drink(price: 12, count: 3)]
+  
+  
+  func getTheDrink(withName: String) throws {
+    guard var drink = drinksInFridge[withName] else {
+      throw PossibleErrors.notInStock
+    }
+    
+    guard drink.count > 0 else {
+      throw PossibleErrors.notInStock
+    }
+    
+    guard drink.price <= deposit else {
+      throw PossibleErrors.notEnoughMoney
+    }
+    
+    deposit -= drink.price
+    drink.count -= 1
+    drinksInFridge[withName] = drink
+    print("You got the Drink: \(withName)")
+    
+  }
+}
+
+let fridge = Fridge()
+try? fridge.getTheDrink(withName: "Pepsi")
+fridge.deposit
+fridge.drinksInFridge
+
+do {
+  try fridge.getTheDrink(withName: "Pepsi")
+} catch PossibleErrors.notInStock {
+  print("Drink is not in stock")
+} catch PossibleErrors.notEnoughMoney {
+  print("Not enough money")
+}
